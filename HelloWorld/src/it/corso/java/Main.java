7a813d139bd2f89f6804a9d7261a4f09bc041f49
janguar <vikj93@live.it>
2022-07-12 22:14:13 +0200

    first project and screens

---
+++ HelloWorld/src/it/corso/java/Main.java
@@ -0,0 +1,20 @@
+package it.corso.java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+		Method1();
+		Method2();
+	}
+	
+	private static void Method1() {
+		System.out.println("Method 1");
+	}
+	
+	private static void Method2() {
+		System.out.println("Method 2");
+	}
+
+}
